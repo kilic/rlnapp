c9861199eb1898842006a87cc46894ef5d0ff872
kilic <kiliconu@itu.edu.tr>
2020-04-25 22:00:20 +0300

    initial repo with contract dev suite

---
+++ contracts/contracts/RLN.sol
@@ -0,0 +1,6 @@
+pragma solidity 0.5.16;
+
+
+contract RLN {
+	constructor() public {}
+}
---
+++ contracts/test-environment.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  accounts: {
+    amount: 10,
+    ether: 100
+  },
+
+  contracts: {
+    type: "truffle",
+    artifactsDir: "build/contracts"
+  },
+  blockGasLimit: 8e6
+};
