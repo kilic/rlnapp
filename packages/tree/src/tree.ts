505d983249f402b9675d184ed95a232bdc312612
kilic <kiliconu@itu.edu.tr>
2020-05-07 11:07:47 +0300

    add batch merkle process lib

--- packages/tree/src/tree.ts
+++ packages/tree/src/tree.ts
@@ -4,6 +4,20 @@ type Level = { [node: number]: Node };
 export type Data = string;
 export type Success = number;
 
+function log2(M: number): number {
+	let depth = 0;
+	let m = M;
+	for (let i = 1; ; i++) {
+		// depth = log2(M)
+		m = m >> 1;
+		if ((m & 1) == 1) {
+			depth = i;
+			break;
+		}
+	}
+	return depth;
+}
+
 export class Tree {
 	private readonly zeros: Array<Node>;
 	private readonly tree: Array<Level> = [];
@@ -34,41 +48,111 @@ export class Tree {
 		if (leafIndex >= this.setSize) {
 			return -1;
 		}
-		this.updateWithData(leafIndex, data);
+		this.doUpdate(leafIndex, this.hasher.hash(data));
+		return 0;
+	}
+
+	public update(leafIndex: number, leaf: Node): Success {
+		if (leafIndex >= this.setSize) {
+			return -1;
+		}
+		this.doUpdate(leafIndex, leaf);
 		return 0;
 	}
 
 	public insertBatch(offset: number, dataBatch: Array<Data>): Success {
-		const M = dataBatch.length;
-		if (M == 0) return -1;
-		if (((M - 1) & M) != 0) return -2;
-		if (M > this.setSize) return -3;
-		if (offset % M != 0) return -4;
-		if (offset >= this.setSize) return -5;
-
-		let depth = 0;
-		let m = M;
-		for (let i = 1; ; i++) {
-			// depth = log2(M)
-			m = m >> 1;
-			if ((m & 1) == 1) {
-				depth = i;
-				break;
-			}
+		const depth = log2(dataBatch.length);
+		const check = this.checkBatch(offset, depth, dataBatch.length);
+		if (check != 0) {
+			return check;
 		}
-
-		// unseccesful if targets non zero node
-		if (!this.isZero(this.depth - depth, offset >> (M - 1))) return -6;
-
-		// make dense merkle
-		// calculate leafs
 		const leafBatch = dataBatch.map((data) => this.hasher.hash(data));
-		// update leafs
-		for (let i = 0; i < M; i++) {
+		for (let i = 0; i < dataBatch.length; i++) {
 			this.tree[this.depth][offset + i] = leafBatch[i];
 		}
-		// ascend dense merkle tree
-		m = M;
+		this.doUpdateBatch(offset, depth, dataBatch.length);
+		return 0;
+	}
+
+	public updateBatch(offset: number, leafBatch: Array<Node>): Success {
+		const depth = log2(leafBatch.length);
+		const check = this.checkBatch(offset, depth, leafBatch.length);
+		if (check != 0) {
+			return check;
+		}
+		for (let i = 0; i < leafBatch.length; i++) {
+			this.tree[this.depth][offset + i] = leafBatch[i];
+		}
+		this.doUpdateBatch(offset, depth, leafBatch.length);
+		return 0;
+	}
+
+	// public updateWithTree(offset: number, tree: Tree): Success {
+	// 	return 0;
+	// }
+
+	// public insertBatch(offset: number, dataBatch: Array<Data>): Success {
+	// 	const M = dataBatch.length;
+	// 	if (M == 0) return -1;
+	// 	if (((M - 1) & M) != 0) return -2;
+	// 	if (M > this.setSize) return -3;
+	// 	if (offset % M != 0) return -4;
+	// 	if (offset >= this.setSize) return -5;
+
+	// 	let depth = 0;
+	// 	let m = M;
+	// 	for (let i = 1; ; i++) {
+	// 		// depth = log2(M)
+	// 		m = m >> 1;
+	// 		if ((m & 1) == 1) {
+	// 			depth = i;
+	// 			break;
+	// 		}
+	// 	}
+
+	// 	// unseccesful if targets non zero node
+	// 	if (!this.isZero(this.depth - depth, offset >> (M - 1))) return -6;
+
+	// 	// make dense merkle
+	// 	// calculate leafs
+	// 	const leafBatch = dataBatch.map((data) => this.hasher.hash(data));
+	// 	// update leafs
+	// 	for (let i = 0; i < M; i++) {
+	// 		this.tree[this.depth][offset + i] = leafBatch[i];
+	// 	}
+	// 	// ascend dense merkle tree
+	// 	m = M;
+	// 	let offsetAscending = offset;
+	// 	for (let i = 0; i < depth; i++) {
+	// 		for (let j = 0; j < 1 << (depth - i); j += 2) {
+	// 			let leafIndex = offsetAscending + j;
+	// 			const level = this.depth - i;
+	// 			const n = this.hashCouple(this.depth - i, leafIndex);
+	// 			leafIndex >>= 1;
+	// 			this.tree[level - 1][leafIndex] = n;
+	// 		}
+	// 		offsetAscending >>= 1;
+	// 		m -= 1;
+	// 	}
+	// 	this.ascend(this.depth - depth, offsetAscending);
+	// 	return 0;
+	// }
+
+	private doUpdate(leafIndex: number, leaf: Node) {
+		this.tree[this.depth][leafIndex] = leaf;
+		this.ascend(this.depth, leafIndex);
+	}
+
+	private ascend(from: number, leafIndex: number) {
+		for (let level = from; level > 0; level--) {
+			const n = this.hashCouple(level, leafIndex);
+			leafIndex >>= 1;
+			this.tree[level - 1][leafIndex] = n;
+		}
+	}
+
+	private doUpdateBatch(offset: number, depth: number, length: number) {
+		let m = length;
 		let offsetAscending = offset;
 		for (let i = 0; i < depth; i++) {
 			for (let j = 0; j < 1 << (depth - i); j += 2) {
@@ -85,31 +169,6 @@ export class Tree {
 		return 0;
 	}
 
-	private getNode(level: number, index: number): Node {
-		return this.tree[level][index] || this.zeros[level];
-	}
-
-	private getLeaf(index: number): Node {
-		return this.tree[this.depth][index] || this.zeros[this.depth];
-	}
-
-	private updateWithData(leafIndex: number, data: Data) {
-		this.updateWithLeaf(leafIndex, this.hasher.hash(data));
-	}
-
-	private updateWithLeaf(leafIndex: number, leaf: Node) {
-		this.tree[this.depth][leafIndex] = leaf;
-		this.ascend(this.depth, leafIndex);
-	}
-
-	private ascend(from: number, leafIndex: number) {
-		for (let level = from; level > 0; level--) {
-			const n = this.hashCouple(level, leafIndex);
-			leafIndex >>= 1;
-			this.tree[level - 1][leafIndex] = n;
-		}
-	}
-
 	private hashCouple(level: number, leafIndex: number) {
 		const X = this.getCouple(level, leafIndex);
 		return this.hasher.hash2(X.l, X.r);
@@ -123,7 +182,23 @@ export class Tree {
 		};
 	}
 
+	private getNode(level: number, index: number): Node {
+		return this.tree[level][index] || this.zeros[level];
+	}
+
 	private isZero(level: number, leafIndex: number): boolean {
 		return this.zeros[level] == this.getNode(level, leafIndex);
+	}
+
+	private checkBatch(offset: number, depth: number, length: number): Success {
+		const M = length;
+		if (M == 0) return -1;
+		if (((M - 1) & M) != 0) return -2;
+		if (M > this.setSize) return -3;
+		if (offset % M != 0) return -4;
+		if (offset >= this.setSize) return -5;
+		if (1 << depth != length) return -6;
+		if (!this.isZero(this.depth - depth, offset >> (M - 1))) return -7;
+		return 0;
 	}
 }
--- packages/tree/test/tree.test.ts
+++ packages/tree/test/tree.test.ts
@@ -23,6 +23,16 @@ describe('Merkle Tree', function () {
 		assert.equal(0, tree32.insert(6, '0x02'));
 		assert.equal(tree32.root, '0x2efd616a1e172e5700aa002b9b41fa3ebe04f61506aa7fff952b1e37ffec12ff');
 	});
+	it('update single', async () => {
+		const tree3 = Tree.new(3, hasher);
+		assert.equal(0, tree3.update(5, hasher.hash('0x01')));
+		assert.equal(0, tree3.update(6, hasher.hash('0x02')));
+		assert.equal(tree3.root, '0x170990af0d57e5ac9161ede7b278c743925bcbb51a1252907a290432cb5bf502');
+		const tree32 = Tree.new(32, hasher);
+		assert.equal(0, tree32.update(5, hasher.hash('0x01')));
+		assert.equal(0, tree32.update(6, hasher.hash('0x02')));
+		assert.equal(tree32.root, '0x2efd616a1e172e5700aa002b9b41fa3ebe04f61506aa7fff952b1e37ffec12ff');
+	});
 	it('insert batch', async () => {
 		const depth = 32;
 		const M = 64;
@@ -35,6 +45,21 @@ describe('Merkle Tree', function () {
 		const otherTree = Tree.new(depth, hasher);
 		for (let i = 0; i < data.length; i++) {
 			otherTree.insert(offset + i, data[i]);
+		}
+		assert.equal(tree.root, otherTree.root);
+	});
+	it('update batch', async () => {
+		const depth = 32;
+		const M = 64;
+		const tree = Tree.new(depth, hasher);
+		const batch = Array<Data>(M)
+			.fill('0')
+			.map((_, i) => hasher.hash(i.toString()));
+		const offset = M * 5;
+		assert.equal(0, tree.updateBatch(offset, batch));
+		const otherTree = Tree.new(depth, hasher);
+		for (let i = 0; i < batch.length; i++) {
+			otherTree.update(offset + i, batch[i]);
 		}
 		assert.equal(tree.root, otherTree.root);
 	});
