b23a1b02cc2f2a6dbd9573d7bb85e3052ebe439c
kilic <kiliconu@itu.edu.tr>
2021-01-08 11:23:59 +0300

    remove auth path syncronizer

--- packages/tree/test/provider.ts
+++ packages/tree/test/provider.ts
@@ -1,11 +1,13 @@
-import { Wallet } from 'ethers';
-import { JsonRpcProvider, Provider } from 'ethers/providers';
+import { providers, Signer, Wallet } from 'ethers';
+import { JsonRpcProvider, Provider } from '@ethersproject/providers';
 
 const URL = 'http://127.0.0.1:8545';
 const downURL = 'http://127.0.0.1:9545';
-const MNEMONIC = 'myth like bonus scare over problem client lizard pioneer submit female collect';
+// const MNEMONIC = 'myth like bonus scare over problem client lizard pioneer submit female collect';
+const MNEMONIC = 'test test test test test test test test test test test junk';
+const PATH = "m/44'/60'/0'/0";
 
-export const provider: Provider = new JsonRpcProvider(URL);
-export const downProvider: Provider = new JsonRpcProvider(downURL);
+export const provider = new JsonRpcProvider(URL);
+export const downProvider = new JsonRpcProvider(downURL);
 (provider as JsonRpcProvider).pollingInterval = 250;
-export const wallet = Wallet.fromMnemonic(MNEMONIC).connect(provider);
+export const wallet = Wallet.fromMnemonic(MNEMONIC, PATH).connect(provider);
